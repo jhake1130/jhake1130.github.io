466e6d1cc6f4943a99f3a40c787ee47f62fe7122
jhake1130 <jirgyh@gmail.com>
2018-04-03 03:10:52 -0600

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){       
    var scroll_start = 0;
-   var startchange = $('.jumbotron');
-   var offset = startchange.offset();
+   var startchange = $('.starthere');
+   var p2offset = startchange.offset();
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
+      if(scroll_start > p2offset.top) {
           $('.topnav').css('background-color', '#000');
        } else {
           $('.topnav').css('background-color', 'transparent');
        }
    });
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function darktheme() {
+  var x = document.getElementById("bodydark");
+   if (x.className === "classbody") {
+        x.className += " responsive";
+    } 
+      else {
+        x.className = "classbody";
+    }
+
+}
