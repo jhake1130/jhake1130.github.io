1afaaae33e5a33386de637483965e80352b4dbd4
jhake1130 <jirgyh@gmail.com>
2018-04-16 01:26:09 -0600

    4/16 version

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var foods =(localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')): {
 	name: [],
 	calories: [],
-	database: [],
 	totalc: 0
 };
 var limit = 1500;
@@ -15,68 +14,56 @@ function dataObjectUpdated() {
 
 		renderList();
 		
-function renderTodoList() {
-	var calorielist = document.getElementsByClassName('tolist')
-	for(var i=0; i< foods.calories.length; i++) {
-		calorielist[i].style.display = "none";  
-	}
- 
-
-}
 
 	function renderList() {
-		var lh2 =  document.getElementById("cl");
-		
-		var calsleft = document.createTextNode(foods.totalc + " Calories")
-
-		lh2.appendChild(calsleft)
-			// var howmany = foods.calories.reduce(function(total, num) {
+		if(foods.calories.length !==0) {
+		var howmany = foods.calories.reduce(function(total, num) {
 
 
-			// 	console.log(total + "total")
-			// 	console.log(num + "num")
-			// foods.totalc = total + num
-			// console.log(foods.totalc + "totalc")
-			// return total + num;
-			// })
-		
-	// for(var i =0; i < todos.length; i++) {
-	// 	var list = document.createElement("li");
-	// 	var note = document.createTextNode(todos[i]);      
-	// 	list.appendChild(note);                                          
-	// 	document.getElementById("todos").appendChild(list);
-	// }
+			// console.log(total + "total") // debugging
+			// console.log(num + "num")  // debugging
+			foods.totalc = total + num
+			// console.log(foods.totalc + "totalc")  // debugging
+			return total + num;
+			})
+	}
+		renderCalHeader();
+
+	
+	for(var i =0; i < todos.length; i++) {
+		var list = document.createElement("li");
+		var note = document.createTextNode(todos[i]);      
+		list.appendChild(note);                                          
+		document.getElementById("todos").appendChild(list);
+	}
 
 	for (var i = 0; i < foods.calories.length; i++) {
 
     var foodname = foods.name[i];
     var foodcals = foods.calories[i];
+
     addItemToDOM(foodname,foodcals);
   }
+  if(foods.name.length ==0) {
+  				var x = document.getElementById("rightside");
+  				x.style.display = "none";  
+  			}
+
+
 	}
 
-document.getElementById('new').addEventListener('click', function() {
-  var value = document.getElementById('foodValue').value;
-  if (value) {
-    newFood(value);
-  }
-});
 
 		function newFood(food) {
 			
-			if (food == 0) {
-				return false;
-			}
-			if(food ==="order 66") {
-				renderTodoList();
-				console.log(food + " Executed")
-			}
 			if(food.indexOf(',') >-1) {
 				var array = food.split (",")
 				var name = array[0];
 				var calories = array[1]
 			}
-			
+			else {
+				alert("Separate food name and calories with a comma")
+				return false;
+			}
 			foods.name.push(name);
 			foods.calories.push(Number(calories));
 			var fname = array[0];
@@ -90,6 +77,8 @@ document.getElementById('new').addEventListener('click', function() {
 				return total + num;
 			})
 
+			renderCalHeader();
+
 			addItemToDOM(fname,cals);
 			
 			array.length= 0;
@@ -99,6 +88,7 @@ document.getElementById('new').addEventListener('click', function() {
 		}
 
 		function addItemToDOM(food,cals) {
+			
 			var list = document.createElement("li"); 
 			list.className = "tolist";
 			var item = document.createElement("div"); 
@@ -110,12 +100,6 @@ document.getElementById('new').addEventListener('click', function() {
 			var buttons = document.createElement('div');
   			buttons.classList.add('buttons');
 			var remove = document.createElement('button');
-			var database = document.createElement('button')
-			database.classList.add('database')
-			database.classList.add("btn")
-			database.classList.add("btn-outline-database")
-			// database.addEventListener('click', dataItem);
-			database.innerText = "M";
 
   			remove.classList.add('remove');
   			remove.classList.add("btn")
@@ -123,63 +107,91 @@ document.getElementById('new').addEventListener('click', function() {
   			remove.addEventListener('click', removeItem);
   			remove.innerText = "X";
   			buttons.appendChild(remove);
-  			buttons.appendChild(database);
+
 			var foodCals = document.createTextNode(cals);
 			var foodName = document.createTextNode(food);
 			item.appendChild(foodName); 
 			counter.appendChild(foodCals);
 			list.appendChild(item);  
 			list.appendChild(counter)
-		       list.appendChild(buttons);
+		    list.appendChild(buttons);
 
 		    
 			document.getElementById("ultodos").appendChild(list);  
 			
 		}
+//
 
-		// function dataItem() {
-		// 	var item = this.parentNode.parentNode;
-  // 			var parent = item.parentNode;
-  // 			var id = parent.class;
-  // 			var value = item.innerText;
-
-
-  // 			if (id === 'tolist') {
-  //   			foods.name.splice(foods.name.indexOf(value), 1);
-  //   			foods.database.push(value);
-  // 			} else {
-  //   			foods.database.splice(foods.database.indexOf(value), 1);
-  //   			foods.name.push(value);
-  // 			}
-  // 			dataObjectUpdated();
-  // 			var target = (id === 'tolist') ? document.getElementById('completed'):document.getElementById('todo');
-
- 
-  // target.insertBefore(item, target.childNodes[0]);
-		// }
 		function removeItem() {
-			
+			var maybethis = this;
 			var item = this.parentNode.parentNode;
+			var idk = this.parentNode;
+
   			var parent = item.parentNode;
   			var id = parent.id;
+  			
   			var value = item.innerText;
 
+
+
+  			var getcals = value.replace(/[^0-9]/g,'');
+  			foods.totalc = foods.totalc - getcals;
+
+  			console.log(maybethis + "maybethis")
+  			console.log(item + "item")
+  			console.log(idk + "idk")
+  			console.log(parent + "parent")
+  			console.log(id + "id")
+  			console.log(value + "value")
+
+
     		foods.name.splice(foods.name.indexOf(value), 1);
-  			var remove = foods.calories.splice(foods.calories.indexOf(value), 1);
+			foods.calories.splice(foods.calories.indexOf(value), 1);
+
+			// 
+			// For replacing the header with current calorie count
+			renderCalHeader();
+  			// 
   			
 
-  			console.log(value + id + item)
   			parent.removeChild(item);
   			if(foods.name.length <= 0) {
   				foods.totalc = 0;
 
   			}
+  			if(foods.name.length <=0) {
+  				var x = document.getElementById("rightside");
+  				x.style.display = "none";  
+  			}
+  			// renderList();
+  			
   			dataObjectUpdated();
 		}
 
+document.getElementById('new').addEventListener('click', function() {
+  var value = document.getElementById('foodValue').value;
+  if (value) {
+    newFood(value);
+  }
+});
 		document.getElementById('delete2').addEventListener('click', function() {
 		var btnshow = document.getElementsByClassName("buttons");
    			for (var i = 0; i < btnshow.length; i++) {
       		btnshow[i].style.display = "block";    
     	}
  		})
+
+
+function renderCalHeader() {
+	var mab = document.getElementsByClassName("limit");
+   			for (var i = 0; i < mab.length; i++) {
+      		mab[i].style.display = "none"; }   
+			document.getElementsByClassName("limit").innerHTML = "";
+
+			var limit=  document.createElement("p")
+			limit.className = "limit"
+			var leemet = document.createTextNode(foods.totalc + " Calories")
+			limit.appendChild(leemet)
+			document.getElementById("calorieheader").appendChild(limit)
+
+}
